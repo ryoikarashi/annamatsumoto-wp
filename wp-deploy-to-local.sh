1c30496b104c7c4e420ba225b6c5068e584c7b3d
Ryo Ikarashi <ryo.5630@gmail.com>
2017-12-07 08:59:38 +0900

    fix loading style

--- wp-deploy-to-local.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/zsh
-
-# Theme
-rsync -avzr --delete dist/theme/* ../../data/annamatsumoto/wp-content/themes/annamatsumoto.com
-
-# Assets
-rsync -avzr --delete dist/assets/* ../../data/annamatsumoto/assets
